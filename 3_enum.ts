f52b55dcd8851ade9153b0c59734a0336b8a17d0
AlexandrKhom <alex988andr@gmail.com>
2021-11-02 01:30:44 +0200

    1

--- 3_enum.ts
+++ 3_enum.ts
@@ -9,6 +9,15 @@ const membershipReverse = Membership[2]
 console.log(membership)
 console.log(membershipReverse)
 
+enum Media {
+    VK = 'VK',
+    FB = 'FB',
+    INSTA = 'INSTA'
+}
+
+const social = Media.INSTA
+console.log(social)
+
 
 
 
