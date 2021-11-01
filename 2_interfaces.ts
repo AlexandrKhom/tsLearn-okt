159e02ae5ac1deec732f1a5dbe0b7e854ade5152
AlexandrKhom <alex988andr@gmail.com>
2021-11-02 01:23:03 +0200

    1

--- 2_interfaces.ts
+++ 2_interfaces.ts
@@ -23,19 +23,37 @@ const rect2: Rect = {
     }
 }
 rect2.color = '#000000'
+// rect2.id = '3' //readonly !!!
 
 const rect3 = {} as Rect
 const rect4 = <Rect>{}
 
+///////////////////////////
 
+interface RectArea extends Rect {
+    getArea: ()=> number
+}
 
+const rect5: RectArea = {
+    id: '5',
+    size: {
+        height: 1,
+        width: 1
+    },
+    getArea(): number {
+        return this.height * this.width
+    }
+}
 
+///////////////////////
 
-
-
-
-
-
+interface Styles {
+    [key: string]: string
+}
+const css: Styles = {
+    border: '1px',
+    radius: '5'
+}
 
 
 
