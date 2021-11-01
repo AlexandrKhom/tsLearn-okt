fd1e067aab8ca999ae4bc96ab060d432d62d2d4c
AlexandrKhom <alex988andr@gmail.com>
2021-11-02 00:57:47 +0200

    1

--- types.ts
+++ types.ts
@@ -1,7 +1,36 @@
 const str: string = 'hello'
-console.log(str)
+// console.log(str)
 
-// const isLoad: boolean = true;
-// const isFeth: boolean = false;
-//
-// const int: number = 11
+const isLoad: boolean = true;
+const isFeth: boolean = false;
+
+const int: number = 11
+const float: number = 4.2
+
+const numArr: number[] = [1,2,3,4,5]
+const numArr2: Array<number> = [1,2,3,4,5]
+
+const contact: [string, number] = ['Alex', 22]
+
+let variable: number|string = 42
+variable = 'alex'
+let variable2: any = 42
+variable2 = 'alex'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
