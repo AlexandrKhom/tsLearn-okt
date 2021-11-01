f2e5570a8fa27664fa88f735222523e53e6c8796
AlexandrKhom <alex988andr@gmail.com>
2021-11-02 01:27:54 +0200

    1

---
+++ 3_enum.ts
@@ -0,0 +1,25 @@
+enum Membership {
+    Simple,
+    Standard,
+    Premium
+}
+
+const membership = Membership.Standard
+const membershipReverse = Membership[2]
+console.log(membership)
+console.log(membershipReverse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
