8c9077aea590f9341c53ba6c77a8e148db943284
AlexandrKhom <alex988andr@gmail.com>
2021-11-02 13:47:19 +0200

    1

---
+++ 8_operators.ts
@@ -0,0 +1,29 @@
+interface Person {
+    name: string
+    age: number
+}
+type PersonKeys = keyof Person //'name' | 'age'
+
+let key: PersonKeys = 'name'
+key = 'age'
+//key = 'email' // wrong
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
