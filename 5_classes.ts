2c3b85a4f6be28e0c1e4c74926cd5ba0a4ce5273
AlexandrKhom <alex988andr@gmail.com>
2021-11-02 13:29:35 +0200

    1

--- 5_classes.ts
+++ 5_classes.ts
@@ -11,6 +11,27 @@ class Car1 {
     }
 }
 
+class Animal {
+    protected voice: string = ''
+    public color: string = 'black'
+    private go() {
+        console.log('GO')
+    }
+}
+class Cat extends Animal{
+    public setVoice(voice:string): void {
+        this.voice = voice
+    }
+}
+///////////////////////
+
+
+
+
+
+
+
+
 
 
 
